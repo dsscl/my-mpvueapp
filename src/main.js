9ebe2c8f09eda9d383edb1812158cd20e4303fb4
unknown <619178495@qq.com>
2019-04-20 18:39:27 +0800

    updated: 云开发初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import MpvueRouterPatch from 'mpvue-router-patch'
 import App from './App'
+import WXP from '@/utils/wxp'
+import store from '@/store/index'
 import '@/assets/iconfont/iconfont.css'
 import '@/assets/sass/sass_index.scss'
 
@@ -8,8 +10,18 @@ Vue.config.productionTip = false
 Vue.use(MpvueRouterPatch)
 App.mpType = 'app'
 
+wx.cloud.init({
+    traceUser: true
+})
+
+Vue.prototype.WXP = WXP
+Vue.prototype.$store = store
+Vue.prototype.db = wx.cloud.database({
+  env: 'dev'
+})
 const app = new Vue(App)
 app.$mount()
+
 
 export default {
 }
